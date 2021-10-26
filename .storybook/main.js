58e253c238c08d86b5649bed0750e04ebdb57afa
zhiqiang.tang <zhiqiang.tang@ecarxgroup.com>
2021-10-26 22:20:21 +0800

    feat: first commit
    first commit ,test
    
    BREAKING CHANGE: first commit

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+    //如何查找故事 story book
+    stories: [
+        "../src/Introduction.stories.mdx",
+        "../src/Install.stories.mdx",
+        "../src/Components.stories.mdx",
+        "../src/**/*.stories.mdx",
+        "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    ],
+    addons: ['@storybook/addon-essentials', '@storybook/addon-postcss'],
+};
---
+++ src/index.tsx
@@ -0,0 +1,3 @@
+import Button from './components/button';
+export type { ButtonProps } from './components/button';
+export { Button };
