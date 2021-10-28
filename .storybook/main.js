c49b6848686ce5e09dc83b13179d1b7e5d7b2b0e
XYis13774785893s <18876309817@163.com>
2021-10-28 13:52:26 +0800

    fix: change

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,5 +7,12 @@ module.exports = {
         "../src/**/*.stories.mdx",
         "../src/**/*.stories.@(js|jsx|ts|tsx)"
     ],
-    addons: ['@storybook/addon-essentials'],
+    addons: ['@storybook/addon-essentials', {
+        name: '@storybook/addon-postcss',
+        options: {
+            postcssLoaderOptions: {
+                implementation: require('postcss'),
+            },
+        },
+    },],
 };
